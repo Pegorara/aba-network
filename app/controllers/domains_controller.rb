dce430b55ee1a8cf06437bbc49e659c48ed9f438
paulamespindola <paulamespindola04@gmail.com>
2024-10-30 11:24:23 -0300

    Modify controller

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,22 +1,16 @@
 class DomainsController < ApplicationController
-
   def index
     @domains = policy_scope(Domain)
     render json: @domains
   end
 
   def show
-    authorize_domain
+    authorize domain
     render json: domain
   end
 
   private
-
   def domain
     @domain ||= Domain.find(params[:id])
-  end
-
-  def authorize_domain
-    authorize domain
   end
 end
