aa512ce95d212c4f6c53d48cbc840934060f31fc
paulamespindola <paulamespindola04@gmail.com>
2024-11-14 22:13:31 -0300

    Add spaces before render

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,29 +2,34 @@ class CompaniesController < ApplicationController
 
   def index
     @companies = policy_scope(Company)
+
     render json: @companies.map { |company| CompanySerializer.call(company) }
   end
 
   def show
     authorize Company
+    
     render json: CompanySerializer.call(company)
   end
 
   def create
     authorize Company, :create?
     @company = Company.create(permitted_attributes(Company))
+
     render json: CompanySerializer.call(company), status: :created
   end
 
   def update
     authorize Company
     company.update!(permitted_attributes(Company))  
+
     render json: CompanySerializer.call(company), status: :ok
   end
 
   def destroy
     authorize Company
     company.destroy!
+
     head :no_content
   end
 
